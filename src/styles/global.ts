9d583c817db2539628a38fa144eece749acfcd01
dashdanilo <dashdanilo@gmail.com>
2022-03-05 19:34:51 -0300

    base components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
   --text-light:#363F5F;
   --text-body:#969cB3;
   --shape:#FFFFFF;
+  --green:#33CC95;
 }
 
 *{
@@ -27,7 +28,7 @@ html{
 }
 
 body{
-  background:var(---background);
+  background:var(--background);
   -webkit-font-smoothing:antialiased;
 }
 
